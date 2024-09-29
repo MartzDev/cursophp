758d4eed8840a243d4afed5f7cb267928884bc0a
MartzDev <155332741+MartzDev@users.noreply.github.com>
2024-09-29 15:24:58 -0500

    fundamentos-introduccion

---
+++ fundamentos/introduccion.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= strtoupper('mi primera pagina con php'); ?></title>
+</head>
+
+<body>
+    <h1><?= ucfirst('mi primera pagina con php') ?></h1>
+
+    <p>versi&oacute;n de php <?= PHP_VERSION ?></p>
+</body>
+
+</html>
